2df743968ba3fb7db062f931bc814118956a2410
Carpmenter <nickcarpenter1017@gmail.com>
2019-03-07 15:59:02 -0600

    added user controller | basic functionality

---
+++ GrowUP/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GrowUP.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GrowUP.Controllers
+{
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+        private readonly growUPContext _context;
+
+        public UserController(growUPContext context)
+        {
+            _context = context;
+
+            if (_context.Users.Count() == 0)
+            {
+                // Create a new TodoItem if collection is empty,
+                // which means you can't delete all TodoItems.
+                _context.Users.Add(new User { FirstName = "Nick", LastName = "Carpenter", Username = "NICA" });
+                _context.SaveChanges();
+            }
+        }
+
+        // api/user
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+    }
+}
