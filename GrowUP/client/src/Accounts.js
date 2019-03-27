9e160953d06a8a432afbec6d50bbdebb3017b1ee
Nick Carpenter <nickcarpenter1017@gmail.com>
2019-03-26 20:22:56 -0500

    containers

--- GrowUP/client/src/Accounts.js
+++ GrowUP/client/src/Accounts.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 
 class Accounts extends React.Component {
-
+    render() {
+        return (
+            <div>Accounts</div>
+        );
+    }
 }
 
 export default Accounts;
