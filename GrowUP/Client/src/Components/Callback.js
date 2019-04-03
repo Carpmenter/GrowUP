3f69129c471e62a71c961a40e913b9121b435544
Carpmenter <nickcarpenter1017@gmail.com>
2019-04-03 16:00:14 -0500

    finished auth component | organized

---
+++ GrowUP/Client/src/Components/Callback.js
@@ -0,0 +1,10 @@
+
+import React from 'react';
+
+const Callback = () => (
+  <div className="container">
+    <h4>Loading...</h4>
+  </div>
+);
+
+export default Callback;
--- GrowUP/Client/src/Services/auth.js
+++ GrowUP/Client/src/Services/auth.js
@@ -17,5 +17,41 @@ export default class Auth {
         this.auth0.authorize();
     }
 
+    handleAuthentication = () => {
+        this.auth0.parseHash((err, authResult) => {
+            if (authResult && authResult.accessToken && authResult.idToken) {
+                this.setSession(authResult);
+                history.replace('/home');
+            } else if (err) {
+                history.replace('/home');
+                console.log(err);
+            }
+        });
+    }
+
+    // saves user info in localStorage
+    setSession = (authResult) => {
+        // setting time access token expires
+        let expiresAt = JSON.stringify((authResult.expiresAt * 1000) + new Date().getTime());
+        localStorage.setItem('access_token', authResult.accessToken);
+        localStorage.setItem('id_token', authResult.idToken);
+        localStorage.setItem('expires_at', expiresAt);
+        history.replace('/home');
+    }
     
+      // removes user details from localStorage
+    logout = () => {
+        // Clear access token and ID token from local storage
+        localStorage.removeItem('access_token');
+        localStorage.removeItem('id_token');
+        localStorage.removeItem('expires_at');
+        // navigate to the home route
+        history.replace('/home');
+    }
+
+    isAuthenticated = () => {
+        //verifying access token has expired
+        let expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+        return new Date().getTime() < expiresAt;
+    }
 }
