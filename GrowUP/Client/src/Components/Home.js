c57e05cf5941de9759d47f91081d1246e0253bf0
Carpmenter <nickcarpenter1017@gmail.com>
2019-04-04 15:49:44 -0500

    Added Home screen for a post authentication view

---
+++ GrowUP/Client/src/Components/Home.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import App from './App';
+
+class Home extends React.Component {
+
+    login = () => {
+        this.props.auth.login();
+    }
+
+    logout = () => {
+        this.props.auth.logout();
+    }
+
+    render(){
+
+        const {isAuthenticated} = this.props.auth;
+
+        return (
+            <div>
+                {
+                    isAuthenticated() &&
+                    <div className="container column">
+                        <h5>
+                            You are logged in!{' '}
+                            <a
+                            style={{ cursor: 'pointer' }}
+                            onClick={this.logout}
+                            >
+                            Log Out
+                            </a>.
+                        </h5>
+                        <App />
+                    </div>
+                }
+                {
+                    !isAuthenticated() && (
+                        <div className="container column">
+                            <h5>ReactiveSearch Auth0 Example</h5>
+                            <h5>
+                                You are not logged in! Please{' '}
+                                <a
+                                    style={{ cursor: 'pointer' }}
+                                    onClick={this.login}
+                                >
+                                    Log In
+                                </a>
+                                {' '}to continue.
+                            </h5>
+                            <h6>This is the default <b><code>Home</code></b> component. The <b><code>App</code></b> component will only be visible once you authenticate.</h6>
+                        </div>
+                    )
+                }
+            </div>
+        );
+    }
+}
+
+export default Home;
