b541e641a8b86e1c63c1491de4b31b42a97ee3b7
Carpmenter <nickcarpenter1017@gmail.com>
2019-04-11 19:32:56 -0500

    initial formatting for accounts section

--- GrowUP/Client/src/Accounts.js
+++ GrowUP/Client/src/Accounts.js
@@ -4,9 +4,21 @@ import {withRouter} from 'react-router-dom';
 class Accounts extends React.Component {
     render() {
 
+        var testAccounts = ["Chase Bank.....1087.99", "Bell Bank.....87.69", "Savings Bell Bank.....6,009.24"];
+        const accounts = [];
+
+        for (let i in testAccounts){
+            accounts.push(<div className="row acct-row">{testAccounts[i]}</div>);
+        }
+
+
         return (
             <div id="accounts" className="col-md-4 mx-3 my-3">
-                <div className="container-fluid">Accounts</div>
+                <div className="container-fluid px-0">
+                    <div className="row" id="acct-head">Accounts</div>
+                    <div className="row">Total Balance</div>
+                    {accounts}
+                </div>
             </div>
         );
     }
--- GrowUP/Client/src/App.js
+++ GrowUP/Client/src/App.js
@@ -13,20 +13,20 @@ import Unauthorized from './Unauthorized';
 
 class App extends React.Component {
     render(){
-
-        if (!auth0Client.isAuthenticated()){
-            return (
-                <div id="content">
-                    <header>
-                        <Navigation />
-                        <Route exact path='/callback' component={Callback} />
-                    </header>
-                    <main>
-                        <Unauthorized />
-                    </main>
-                </div>
-            );
-        }
+        // Uncomment for authorized rendering
+        // if (!auth0Client.isAuthenticated()){
+        //     return (
+        //         <div id="content">
+        //             <header>
+        //                 <Navigation />
+        //                 <Route exact path='/callback' component={Callback} />
+        //             </header>
+        //             <main>
+        //                 <Unauthorized />
+        //             </main>
+        //         </div>
+        //     );
+        // }
         
         return (
             <div id="content">
@@ -36,11 +36,11 @@ class App extends React.Component {
             </header>
             <main>
                 <div id="main" className="container-fluid">
-                    <div className="row">
+                    <div className="row justify-content-center">
                         <Chart />
                         <Accounts />
                     </div>
-                    <div className="row">
+                    <div className="row justify-content-center">
                         <Expenses />
                         <Earnings />
                     </div>
