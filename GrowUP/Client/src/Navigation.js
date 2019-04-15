488def20406b15b6f5a6dd7eb97770bd419a1d5f
Carpmenter <nickcarpenter1017@gmail.com>
2019-04-15 16:41:57 -0500

    troubleshooting navbar collapse

--- GrowUP/Client/src/Navigation.js
+++ GrowUP/Client/src/Navigation.js
@@ -16,31 +16,30 @@ function Navigation(props) {
             </button>
             <div className="collapse navbar-collapse" id="nav">
                 <ul className="navbar-nav">
-                <li className="nav-item px-1">
-                    <a className="nav-link" href="#">Dashboard</a>
-                </li>
-                <li className="nav-item px-1">
-                    <a className="nav-link" href="#">Transactions</a>
-                </li>
-                <li className="nav-item px-1">
-                        <a className="nav-link" href="#">Profile</a>
-                </li>
-                {
-                    !auth0Client.isAuthenticated() &&
-                    <button className="btn btn-light" onClick={auth0Client.signIn}>Sign In</button>
-                }
-                {
-                    auth0Client.isAuthenticated() &&
-                    <li>
-                        <button className="btn btn-light" onClick={() => {signOut()}}>Sign Out {auth0Client.getProfile().name}?</button>
+                    <li className="nav-item px-1">
+                        <a className="nav-link" href="#">Dashboard</a>
                     </li>
-                }
+                    <li className="nav-item px-1">
+                        <a className="nav-link" href="#">Transactions</a>
+                    </li>
+                    <li className="nav-item px-1">
+                            <a className="nav-link" href="#">Profile</a>
+                    </li>
+                    {
+                        !auth0Client.isAuthenticated() &&
+                        <button className="btn btn-light" onClick={auth0Client.signIn}>Sign In</button>
+                    }
+                    {
+                        auth0Client.isAuthenticated() &&
+                        <button className="btn btn-light" onClick={() => {signOut()}}>Sign Out {auth0Client.getProfile().name}?</button>
+                    }
                 </ul>
             </div>
             <a className="navbar-brand" href="#">
                 <img src="./images/logo3.png" id="logo"/>
             </a>
         </nav>
+        
     );
 }
 
