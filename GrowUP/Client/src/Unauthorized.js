6affdf6b0c65ee94b6e8234420d8e64d8b391ee3
Carpmenter <nickcarpenter1017@gmail.com>
2019-04-17 15:47:25 -0500

    updating unauthorized screen

--- GrowUP/Client/src/Unauthorized.js
+++ GrowUP/Client/src/Unauthorized.js
@@ -7,8 +7,19 @@ function Unauthorized(){
 
     if (!auth0Client.isAuthenticated()){
         return (
-            <div>
-                Need an Account?
+            <div className="container-fluid " id="unauth">
+                <div className="row py-3">
+                    <div>To access features of GrowUP you must be logged in.</div>
+                    <div>
+                        <button className="btn btn-light" onClick={auth0Client.signIn}>Sign In</button>
+                    </div>
+                </div>
+                <div className="row py-3">
+                    <div>Need an Account?</div>
+                    <div>
+                        <button className="btn btn-light" onClick={auth0Client.signIn}>Sign UP</button>
+                    </div>
+                </div>
             </div>
         );
     }
